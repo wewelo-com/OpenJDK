05486fb565a22c7886ed8fa2481f84e71e55ac7c
Kasper Sogaard <kasper@sogaard.us>
2021-03-27 13:59:50 -0700

    Windows stub

--- BuildScript.cmd
+++ BuildScript.cmd
@@ -1,6 +1,3 @@
 @ECHO ON
 IF NOT EXIST .\build mkdir .\build
-
-curl.exe --output build/jdk-15.0.2.zip --location --url https://s3-us-west-2.amazonaws.com/alpha.archipelago.build/jdk-15.0.2.zip
-tar -xf build\jdk-15.0.2.zip -C build\
-del build\jdk-15.0.2.zip
+echo Hello there > build\OpenJDK.txt
